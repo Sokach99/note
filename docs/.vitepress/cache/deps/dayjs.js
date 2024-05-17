1cf177f8f64940b6d3fa3466e07bef860a5561ba
Sokach <140926861+Sokach99@users.noreply.github.com>
2024-05-17 16:16:01 +0800

    移除缓存

--- docs/.vitepress/cache/deps/dayjs.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_dayjs_min
-} from "./chunk-C5AJ6IJO.js";
-import "./chunk-LQ2VYIYD.js";
-export default require_dayjs_min();
-//# sourceMappingURL=dayjs.js.map
--- docs/.vitepress/cache/deps/dayjs_locale_zh-cn.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_zh_cn
-} from "./chunk-NJFQ7QJV.js";
-import "./chunk-C5AJ6IJO.js";
-import "./chunk-LQ2VYIYD.js";
-export default require_zh_cn();
-//# sourceMappingURL=dayjs_locale_zh-cn.js.map
